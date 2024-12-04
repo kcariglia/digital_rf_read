169e69959ff935ebb83b0c945d9260890111911b
Katherine Cariglia <cariglia@mit.edu>
2024-12-04 10:03:25 -0500

    failing test case now last instead of first

--- c/tests/example_rf_read_hdf5.c
+++ c/tests/example_rf_read_hdf5.c
@@ -12,48 +12,7 @@ int main(int argc, char* argv[])
     char * dir2 = "/Users/cariglia/Desktop/drfexamples/hprec_channels";
     char * dir3 = "/Users/cariglia/Desktop/drfexamples/hprec_subchannels";
 
-    printf("Test 1: basic example\n");
-    printf("----------------------\n");
-    //printf("about to init read obj\n");
-
     
-    read_obj = digital_rf_create_read_hdf5(dir1, b);
-    printf("init success\n");
-
-    
-    //printf("about to get channels\n");
-    channels = get_channels(read_obj);
-    
-    channels2 = read_obj->channel_names;
-
-    printf("got channels:\n");
-    num_channels = read_obj->num_channels;
-
-    for (int i = 0; i < num_channels; i++) {
-        printf("%s\n", channels[i]);
-        printf("%s\n", channels2[i]);
-        printf("sample rate: %Lf\n", read_obj->channels[i]->top_level_dir_meta->sample_rate);
-        printf("time desc: %s\n", read_obj->channels[i]->top_level_dir_meta->drf_time_desc);
-        printf("version: %s\n", read_obj->channels[i]->top_level_dir_meta->version);
-        printf("epoch: %s\n", read_obj->channels[i]->top_level_dir_meta->epoch);
-    }
-    fflush(stdout);
-    unsigned long long * bounds1;
-    bounds1 = get_bounds(read_obj, "junk0");
-    printf("got bounds: %llu  %llu\n", bounds1[0], bounds1[1]);
-    fflush(stdout);
-
-    unsigned long ** cont_data_arr1 = NULL;
-    unsigned long long s0, s1;
-    s0 = bounds1[0];
-    s1 = bounds1[1];
-    printf("(main)s0: %llu  s1: %llu\n", s0, s1);
-    printf("got bounds (again): %llu  %llu\n", bounds1[0], bounds1[1]);
-    //cont_data_arr1 = get_continuous_blocks(read_obj, s0, s1, read_obj->channel_names[0]);
-    fflush(stdout);
-    free(bounds1);
-    digital_rf_close_read_hdf5(read_obj);
-    read_obj = NULL;
 
     printf("Test 2: real data, no subchannels\n");
     printf("----------------------\n");
@@ -78,6 +37,12 @@ int main(int argc, char* argv[])
     unsigned long long * bounds2;
     bounds2 = get_bounds(read_obj, "da");
     printf("got bounds: %ld  %ld\n", bounds2[0], bounds2[1]);
+
+    unsigned long long s0, s1;
+    s0 = bounds2[0];
+    s1 = bounds2[1];
+    printf("(main)s0: %llu  s1: %llu\n", s0, s1);
+    printf("got bounds (again): %llu  %llu\n", bounds2[0], bounds2[1]);
 
     long long ** cont_data_arr2 = NULL;
     //cont_data_arr2 = get_continuous_blocks(read_obj, bounds2[0], bounds2[1], read_obj->channel_names[0]);
@@ -115,6 +80,50 @@ int main(int argc, char* argv[])
     //cont_data_arr3 = get_continuous_blocks(read_obj, bounds3[0], bounds3[1], read_obj->channel_names[0]);
 
     free(bounds3);
+    digital_rf_close_read_hdf5(read_obj);
+    read_obj = NULL;
+
+
+    printf("Test 1: basic example\n");
+    printf("----------------------\n");
+    //printf("about to init read obj\n");
+
+    
+    read_obj = digital_rf_create_read_hdf5(dir1, b);
+    printf("init success\n");
+
+    
+    //printf("about to get channels\n");
+    channels = get_channels(read_obj);
+    
+    channels2 = read_obj->channel_names;
+
+    printf("got channels:\n");
+    num_channels = read_obj->num_channels;
+
+    for (int i = 0; i < num_channels; i++) {
+        printf("%s\n", channels[i]);
+        printf("%s\n", channels2[i]);
+        printf("sample rate: %Lf\n", read_obj->channels[i]->top_level_dir_meta->sample_rate);
+        printf("time desc: %s\n", read_obj->channels[i]->top_level_dir_meta->drf_time_desc);
+        printf("version: %s\n", read_obj->channels[i]->top_level_dir_meta->version);
+        printf("epoch: %s\n", read_obj->channels[i]->top_level_dir_meta->epoch);
+    }
+    fflush(stdout);
+    unsigned long long * bounds1;
+    bounds1 = get_bounds(read_obj, channels[0]);//"junk0");
+    printf("got bounds: %llu  %llu\n", bounds1[0], bounds1[1]);
+    fflush(stdout);
+
+    unsigned long ** cont_data_arr1 = NULL;
+   // unsigned long long s0, s1;
+    s0 = bounds1[0];
+    s1 = bounds1[1];
+    printf("(main)s0: %llu  s1: %llu\n", s0, s1);
+    printf("got bounds (again): %llu  %llu\n", bounds1[0], bounds1[1]);
+    //cont_data_arr1 = get_continuous_blocks(read_obj, s0, s1, read_obj->channel_names[0]);
+    fflush(stdout);
+    free(bounds1);
     digital_rf_close_read_hdf5(read_obj);
     read_obj = NULL;
 
